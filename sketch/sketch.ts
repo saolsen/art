0ed4c341db6e7911634cdec2eec515d3ff6c6616
steve olsen <dev@steve.computer>
2020-01-10 18:08:04 -0500

    types

--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -1,25 +1,29 @@
-let tiles = [];
+interface Entity {
+    name: string,
+    cats: number,
+}
+
+let foos: Entity[];
 
 function setup() {
-    // Wait then, is innerWidth, innerHeight wrong?
     let canvas = createCanvas(100, 100);
     canvas.parent('container');
     const parent = document.getElementById('container');
     resizeCanvas(parent.clientWidth, parent.clientHeight, true);
+    foos = [
+        { name: "steve", cats: 7 },
+        { name: "james", cats: 100 }
+    ];
+    console.log(`Number of cats in foos 0 and 1: ${foos[0].cats + foos[1].cats}`);
 }
 
 function windowResized() {
-    // @TODO: Figure out if I need to do something about dpi.
     const parent = document.getElementById('container');
     resizeCanvas(parent.clientWidth, parent.clientHeight, true);
 }
 
 function draw() {
-
-
     let tile_size = width / 32.0;
-    // We're assuming we're 16x9
-
     background(100);
     push();
     fill(255, 0, 0);
