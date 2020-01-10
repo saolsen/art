25c7b7b2175e1068757dfed520a9c1cad4ca07cd
steve olsen <dev@steve.computer>
2020-01-10 15:08:42 -0500

    resize correctly

--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -1,86 +1,29 @@
-let canvas;
-
-let draw_width_4x3 = 0;
-let draw_height_4x3 = 0;
-let draw_width_16x9 = 0;
-let draw_height_16x9 = 0;
-let draw_width_3x2 = 0;
-let draw_height_3x2 = 0;
-
-function setupViewport() {
-    if (windowHeight * 4 / 3 >= windowWidth) {
-        // scale the height
-        draw_width_4x3 = windowWidth;
-        draw_height_4x3 = windowWidth * 3 / 4;
-    } else {
-        // scale the width
-        draw_width_4x3 = windowHeight * 4 / 3;
-        draw_height_4x3 = windowHeight;
-    }
-
-    if (windowHeight * 16 / 9 >= windowWidth) {
-        // scale the height
-        draw_width_16x9 = windowWidth;
-        draw_height_16x9 = windowWidth * 9 / 16;
-    } else {
-        // scale the width
-        draw_width_16x9 = windowHeight * 16 / 9;
-        draw_height_16x9 = windowHeight;
-    }
-
-    if (windowHeight * 3 / 2 >= windowWidth) {
-        // scale the height
-        draw_width_3x2 = windowWidth;
-        draw_height_3x2 = windowWidth * 2 / 3;
-    } else {
-        // scale the width
-        draw_width_3x2 = windowHeight * 3 / 2;
-        draw_height_3x2 = windowHeight;
-    }
-
-    let max_width = max(draw_width_16x9, draw_width_4x3);
-    let max_height = max(draw_height_16x9, draw_height_4x3);
-
-    resizeCanvas(max_width, max_height);
-}
-
-function drawViewportGuide() {
-    push();
-    noFill();
-    stroke(255, 0, 0);
-    rect((width - draw_width_4x3) / 2,
-        (height - draw_height_4x3) / 2,
-        draw_width_4x3 - 1, draw_height_4x3 - 1);
-    stroke(0, 255, 0);
-    rect((width - draw_width_16x9) / 2,
-        (height - draw_height_16x9) / 2,
-        draw_width_16x9 - 1, draw_height_16x9 - 1);
-    stroke(0, 255, 255);
-    rect((width - draw_width_3x2) / 2,
-        (height - draw_height_3x2) / 2,
-        draw_width_3x2 - 1, draw_height_3x2 - 1);
-    pop();
-}
-
-// Sketch Stuff
 let tiles = [];
 
 function setup() {
-    createCanvas(10, 10);
-    setupViewport();
-    console.log("Hello");
+    // Wait then, is innerWidth, innerHeight wrong?
+    let canvas = createCanvas(100, 100);
+    canvas.parent('container');
+    const parent = document.getElementById('container');
+    resizeCanvas(parent.clientWidth, parent.clientHeight, true);
 }
 
 function windowResized() {
-    setupViewport();
+    // @TODO: Figure out if I need to do something about dpi.
+    const parent = document.getElementById('container');
+    resizeCanvas(parent.clientWidth, parent.clientHeight, true);
 }
 
 function draw() {
+
+
+    let tile_size = width / 32.0;
+    // We're assuming we're 16x9
+
     background(100);
     push();
     fill(255, 0, 0);
     rect(10, 10, 100, 100);
     pop();
     rect(200, 150, 100, 100);
-    drawViewportGuide();
 }
