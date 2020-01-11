537da44c40cc1a15c0fb28573d330c069b7a59f1
steve olsen <steve@steve.computer>
2020-01-11 15:31:15 -0500

    cuber did the media queries!

--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -3,27 +3,32 @@ let tiles = [];
 function setup() {
     // Wait then, is innerWidth, innerHeight wrong?
     let canvas = createCanvas(100, 100);
-    canvas.parent('container');
-    const parent = document.getElementById('container');
+    canvas.parent('aspect-ratio');
+    const parent = document.getElementById('aspect-ratio');
     resizeCanvas(parent.clientWidth, parent.clientHeight, true);
 }
 
 function windowResized() {
     // @TODO: Figure out if I need to do something about dpi.
-    const parent = document.getElementById('container');
+    const parent = document.getElementById('aspect-ratio');
     resizeCanvas(parent.clientWidth, parent.clientHeight, true);
 }
 
 function draw() {
-
-
     let tile_size = width / 32.0;
     // We're assuming we're 16x9
-
-    background(100);
+    //background(100);
     push();
-    fill(255, 0, 0);
-    rect(10, 10, 100, 100);
+    fill(130, 255, 42);
+    text("Hello Cuber!", width / 2 - 30, height / 2);
     pop();
-    rect(200, 150, 100, 100);
+    push();
+    fill(10, 25, 133);
+    rect(0, 0, tile_size, tile_size);
+    rect(width - tile_size, 0, tile_size, tile_size);
+    rect(0, height - tile_size, tile_size, tile_size);
+    rect(width - tile_size, height - tile_size, tile_size, tile_size);
+    rect(0, 0, tile_size, tile_size);
+    rect(0, 0, tile_size, tile_size);
+    pop();
 }
